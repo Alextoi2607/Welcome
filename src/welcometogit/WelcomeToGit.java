7f40f43be7eca5d849fce9ca7946ae8e43c8cb26
Alex <Alex@DESKTOP-56BIJJN>
2019-05-20 12:37:28 +0200

    second commit

--- src/welcometogit/WelcomeToGit.java
+++ src/welcometogit/WelcomeToGit.java
@@ -16,6 +16,7 @@ public class WelcomeToGit {
      */
     public static void main(String[] args) {
         System.out.println("welcome to git");
+        System.out.println("welcome to git x2");
     }
     
 }
